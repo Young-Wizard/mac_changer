50193966661471e7ca0cd75443c255343ec06576
youngwizard.js <adebowaleolamilekan@yahoo.com>
2020-06-16 07:04:07 +0100

    Update mac_changer.py

--- mac_changer.py
+++ mac_changer.py
@@ -24,16 +24,24 @@ def change_mac(interface, mac_address):
     subprocess.call(["ifconfig", interface, "up"])
 
 
+def get_current_mac(interface):
+    ifconfig_result = subprocess.check_output(["ifconfig", interface])
+    mac_address_search = re.search(r"\w\w:\w\w:\w\w:\w\w:\w\w:\w\w", ifconfig_result)
+
+    if mac_address_search:
+        return mac_address_search.group(0)
+    else:
+        print("[+] Could not read mac address")
+
+
 options = get_argument()
+current_mac = get_current_mac(options.interface)
+print("Current MAC = " + str(current_mac))
 
-# change_mac(options.interface, options.mac_address)
+change_mac(options.interface, options.mac_address)
 
-
-ifconfig_result = subprocess.check_output(["ifconfig", options.interface])
-print(ifconfig_result)
-
-mac_address_search = re.search(r"\w\w:\w\w:\w\w:\w\w:\w\w:\w\w", ifconfig_result)
-if mac_address_search:
-    print(mac_address_search.group(0))
+current_mac = get_current_mac(options.interface)
+if current_mac == options.mac_address:
+    print("[+] MAC address was successfully changed to " + current_mac)
 else:
-    print("[+] Could not read mac address")
+    print("[+] MAC address did not get changed.")
